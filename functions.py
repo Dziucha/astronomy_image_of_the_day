9b616f0c9f7cd2b09c4779d6ee2b6ce2269fdc07
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-20 19:00:38 +0100

    displaying proper title, picture and info

--- functions.py
+++ functions.py
@@ -1,9 +1,6 @@
 import requests
 import json
 
-# api = "kUMVV5bF43mbXI6kIRbPMiEhFcjyEABHaSBQ0HCS"
-# url = f"https://api.nasa.gov/planetary/apod?api_key={api}"
-
 
 def download_data(url):
     info = requests.get(url).content
@@ -13,4 +10,5 @@ def download_data(url):
 
 def save_image(image_url):
     with open("image.jpg", "wb") as file:
-        file.write(image_url)
+        image_bites = requests.get(image_url).content
+        file.write(image_bites)
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import functions
+# import requests
 
 api = "kUMVV5bF43mbXI6kIRbPMiEhFcjyEABHaSBQ0HCS"
-url = f"https://api.nasa.gov/planetary/apod?apikey={api}"
+url = f"https://api.nasa.gov/planetary/apod?api_key={api}"
 image_of_the_day = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/2560px-Cat_August_2010-4.jpg"
 
-title = st.title("title")
-
 data = functions.download_data(url)
-# picture_url = data["url"]
 
-# data_frame = functions.extract_data()
+title = st.title(data["title"].title())
 
-# image = st.image("image.jpg")
+picture_url = data["url"]
+functions.save_image(picture_url)
 
-description = st.text("sample description for now it is a string only")
+image = st.image("image.jpg")
+
+description = st.info(data["explanation"])
